a05b96e5efe4dabbc3af0ef367572177424f9b39
Chris Solis <chris.solis86@gmail.com>
2026-02-21 09:14:24 -0800

    Initial Commit

---
+++ TownGreen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TownGreen
+//
+//  Created by Chris Solis on 2/21/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TownGreen/TownGreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  TownGreenApp.swift
+//  TownGreen
+//
+//  Created by Chris Solis on 2/21/26.
+//
+
+import SwiftUI
+
+@main
+struct TownGreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
